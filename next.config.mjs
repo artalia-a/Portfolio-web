f6bbcd9ff29f9b50913f8acca581516b1e647d66
Artini Artalia <artiniketut502@gmail.com>
2025-09-10 11:07:51 +0800

    next.config.mjs update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["127.0.0.1", "localhost"],
+
+//   },
+// };
+
+// export default nextConfig;
 const nextConfig = {
   images: {
-    domains: ["127.0.0.1", "localhost"],
+    domains: [
+      "127.0.0.1",
+      "localhost",
+      "portfolio-api-production-709b.up.railway.app",
+    ],
   },
 };
 
